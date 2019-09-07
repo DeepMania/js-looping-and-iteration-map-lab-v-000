1aba2c64137b648ded847d2a2a5fb3d8e0a4ed06
Yana Ustsinovich <yustinovich@icloud.com>
2019-09-07 17:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ function lowerCaseDrivers (drivers) {
  let lowerCaseNames = drivers.map(function (name) {return name.toLowerCase()})
  return lowerCaseNames;
 }
-function nameToAttributes (drivers) {
-
-  let driversObject = drivers.map(function (name) {for name of drivers
-  {return {drivers[0]: drivers[1]}}})
-  return driversObject;
-}
