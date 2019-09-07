e017a5e8ec54fb648264b8df49d599ccf8930c0d
Yana Ustsinovich <yustinovich@icloud.com>
2019-09-07 17:19:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file.
+function lowerCaseDrivers (drivers) {
+ let lowerCaseNames = drivers.map(function (name) {return name.toLowerCase()})
+ return lowerCaseNames;
+}
+function nameToAttributes (drivers) {
+
+  let driversObject = drivers.map(function (name) {for name of drivers
+  {return {drivers[0]: drivers[1]}}})
+  return driversObject;
+}
